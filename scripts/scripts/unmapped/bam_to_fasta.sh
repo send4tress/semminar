328a0a3f63f6585946f4cb73c22e62a1e9b3f7aa
send4tress <send4tress@gmail.com>
2024-10-16 20:43:22 -0500

    uploaded scripts

---
+++ scripts/scripts/unmapped/bam_to_fasta.sh
@@ -0,0 +1,1 @@
+bedtools bamtofastq -i /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped.bam -fq /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r1.fastq -fq2 /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r2.fastq
---
+++ scripts/scripts/unmapped/unmapped_fastqc.sh
@@ -0,0 +1,2 @@
+fastqc /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r1.fastq -o /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly
+fastqc /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly/unmapped_r2.fastq -o /scratch/biol726308/BIOL7263_Genomics/sequencing_data/ecoli/unmapped_assembly
